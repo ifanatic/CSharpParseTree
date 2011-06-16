e3d70a50579e8c4a9774c1fbcc857363308edf29
ifanatic <onlk@yandex.ru>
2011-06-16 20:44:07 +0600

    simple refactoring and progress indicator added

---
+++ CSharpParseTree.Common/AbstractProjectProcessor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpParseTree.Common
+{
+    public delegate void ProgressHandler(int percent);
+    
+    public abstract class AbstractProjectProcessor
+    {
+        protected virtual void DoProgress(int percent)
+        {
+            if (OnProgress != null)
+            {
+                CurrentProgress = percent;
+                OnProgress(percent);
+            }
+        }
+
+        public virtual void Process(SourceCodeProject project) { }
+
+        public ProgressHandler OnProgress;
+        public int CurrentProgress { get; private set; }
+        
+    }
+}
---
+++ CSharpParseTreeC/ProgressIndicator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpParseTreeC
+{
+    class ProgressIndicator
+    {
+        private int _left;
+        private int _top;
+
+        private const int AREA_SIZE = 5;
+
+        public ProgressIndicator(int left, int top)
+        {
+            _left = left;
+            _top = top;
+        }
+
+        public void OnProgress(int percent)
+        {
+            int currentLeft = Console.CursorLeft;
+            int currentTop = Console.CursorTop;
+
+            Console.SetCursorPosition(_left, _top);
+            Console.Write("{0}%", percent);
+
+            for (int i = (percent.ToString().Length + 1); i <= AREA_SIZE; ++i)
+            {
+                Console.Write(" ");
+            }
+
+            Console.SetCursorPosition(currentLeft, currentTop);
+        }
+    }
+}
